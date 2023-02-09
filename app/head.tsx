3fc7b89dfbed650907ebedd9f534e718eadc876a
Kaushik das <danishsawl6@gmail.com>
2023-02-09 15:38:46 +0530

    P01

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>ChatGpt 2.0</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
